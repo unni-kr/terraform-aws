c9fb6fa67beffb789d0f0ce9ea50dd3c794d852d
krishnanunni <krishnanunni.n.menon@gmail.com>
2020-10-13 07:35:53 +0000

    moved backend to outside of module

--- instance-from-module/main.tf
+++ instance-from-module/main.tf
@@ -12,3 +12,15 @@ module "instances" {
   db_remote_state_table = var.db_remote_state_table
 }
 
+terraform {
+  backend "s3" {
+    shared_credentials_file = "/home/krishnanunni_n_meon/.aws/credentials"
+    # Replace this with your bucket name!
+    bucket         = "crew-terraform-state-bucket"
+    key            = "instance-from-module/terraform.tfstate"
+    region         = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -12,16 +12,3 @@ data "aws_ami" "ubuntu_ami" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 }
-
-terraform {
-  backend "s3" {
-    shared_credentials_file = "/home/krishnanunni_n_meon/.aws/credentials"
-    # Replace this with your bucket name!
-    bucket         = var.db_remote_state_bucket
-    key            = var.db_remote_state_key
-    region         = "us-east-1"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = var.db_remote_state_table
-    encrypt        = true
-  }
-}
